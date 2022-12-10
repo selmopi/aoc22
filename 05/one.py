c005e7d2f5a1c7e7630992d8376c61ccdaae1e5a
josef zerpa <josefzerpa@protonmail.com>
2022-12-10 23:09:49 +0100

    Completed day 05

---
+++ 05/one.py
@@ -0,0 +1,57 @@
+
+def main():
+    file = open("input")
+
+    # List of stacks.
+    stacks = dict()
+
+    # Get the stack map.
+    while line := file.readline() :
+        line = line.strip()
+        
+        # Break when finding empty line.
+        if len(line) == 0:
+            break;
+
+        # Parse stack map.
+        for i in range(1, len(line), 4):
+            id = str(line[i]).strip()
+            
+            if len(id) != 0:
+                n = int(((i - 1) / 4) + 1)
+                stack = stacks.get(n)
+                if stack is None:
+                    stack = list()
+                    stacks[n] = stack
+                stack.append(id)
+
+    # Fix stacks order.
+    for stack in stacks.values():
+        stack.reverse()
+
+    # Get and apply the rearrengement procedure.
+    while line := file.readline() :
+        line = line.strip()
+
+        words = line.split(" ")
+        n = words[1]
+        from_n = words[3]
+        to_n = words[5]
+
+        for i in range(int(n)):
+            from_stack = stacks.get(int(from_n))
+            to_stack = stacks.get(int(to_n))
+            to_stack.append(from_stack.pop())
+
+
+    file.close()
+
+    # Get results.
+    results = ""
+    stack_size = 10
+    for i in range(1, int(stack_size)):
+        results += str(stacks.get(i).pop())
+    print(results)
+
+if __name__ == "__main__":
+    main()
